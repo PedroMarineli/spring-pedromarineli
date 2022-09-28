329f3f503205141fb02c6cc894b34022f996070d
PedroMarineli <phmo.1606@gmail.com>
2022-09-28 17:21:18 +0000

    Entrega

---
+++ app/src/main/java/pedromarineli/repositories/AlunosRepository.java
@@ -0,0 +1,6 @@
+package pedromarineli.repositories;
+import org.springframework.data.repository.CrudRepository;
+import pedromarineli.models.Aluno;
+public interface AlunosRepository extends CrudRepository<Aluno, Integer> {
+
+} 
