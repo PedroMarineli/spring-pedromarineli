d23a02f6b618e85740bcbecafc1e58cb180e0e49
PedroMarineli <phmo.1606@gmail.com>
2022-09-28 17:29:41 +0000

    Entrega

--- app/src/main/java/pedromarineli/controllers/AlunosController.java
+++ app/src/main/java/pedromarineli/controllers/AlunosController.java
@@ -1,32 +1,38 @@
 package pedromarineli.controllers;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
 import pedromarineli.models.Aluno;
 import pedromarineli.repositories.AlunosRepository;
+
 @Controller
 @RequestMapping("/alunos")
 public class AlunosController {
- @Autowired
- private AlunosRepository alunosRepo;
- @RequestMapping("list")
- public String list(Model model) {
- model.addAttribute("alunos", this.alunosRepo.findAll());
- return "list";
- }
- @RequestMapping("insert")
- public String insert() {
- return "insert";
- }
- @RequestMapping(value = "insert", method = RequestMethod.POST)
- public String insert(@RequestParam("nome") String nome, @RequestParam("idade") int idade) {
- Aluno aluno = new Aluno();
- aluno.setNome(nome);
- aluno.setIdade(idade);
- alunosRepo.save(aluno);
- return "redirect:/alunos/list";
- }
+    @Autowired
+    private AlunosRepository alunosRepo;
+
+    @RequestMapping("list")
+    public String list(Model model) {
+        model.addAttribute("alunos", this.alunosRepo.findAll());
+        return "list";
+    }
+
+    @RequestMapping("insert")
+    public String insert() {
+        return "insert";
+    }
+        
+    @RequestMapping(value = "insert", method = RequestMethod.POST)
+    public String insert(@RequestParam("nome") String nome, @RequestParam("idade") int idade) {
+        Aluno aluno = new Aluno();
+        aluno.setNome(nome);
+        aluno.setIdade(idade);
+        alunosRepo.save(aluno);
+        return "redirect:/alunos/list";
+    }
 } 
--- app/src/main/java/pedromarineli/repositories/AlunosRepository.java
+++ app/src/main/java/pedromarineli/repositories/AlunosRepository.java
@@ -1,6 +1,8 @@
 package pedromarineli.repositories;
+
 import org.springframework.data.repository.CrudRepository;
 import pedromarineli.models.Aluno;
+
 public interface AlunosRepository extends CrudRepository<Aluno, Integer> {
 
 } 
